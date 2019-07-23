LineageOS/android_device_oneplus_sdm845-common

sdm845-common: Prebuilt kernel support
Change-Id: I21ec2b8f8e825d0192cef874a20ad7f92673e5b1

BoardConfigCommon.mk
@@ -46,8 +46,10 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+  TARGET_KERNEL_SOURCE := kernel/oneplus/sdm845
+  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+endif
 
 # Platform
 # TARGET_BOARD_PLATFORM := sdm845

KanishkTheDerp/device_xiaomi_sdm845-common

BoardConfigCommon.mk
@@ -36,8 +36,10 @@ BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
 NEED_KERNEL_MODULE_SYSTEM := true
 TARGET_KERNEL_ARCH := arm64
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+  TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
+  TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
+endif
 
 # Platform
 #TARGET_BOARD_PLATFORM := sdm845